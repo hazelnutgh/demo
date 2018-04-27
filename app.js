e977170d7ac2b570093ff765823f17662aa8869f
Adrian Hazelnut <hazelnutgh@gmail.com>
2018-04-27 11:02:14 +0000

    updated routes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes/routes');
+
+app.use('/', routes);
+
 app.use(express.static(__dirname + '/public'));
 
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    
+   res.render('index', {
+       heading: 'Benzino'
+   }) 
+});
+
+router.get('/about', (req, res) => {
+   res.render('about', {
+       heading: 'About:'
+   }) 
 });
 
 module.exports = router;
