c89a816a846fd8f2adf5a6cdf05265c615e85a72
Adrian Hazelnut <hazelnutgh@gmail.com>
2018-04-27 12:26:35 +0000

    hbs settings defined

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
 const routes = require('./routes/routes');
 
 app.use('/', routes);
